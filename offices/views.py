23562211f8213ab1119e0f52f6c3f7fa003d98c2
jethro guzman <jethroguzman@gmail.com>
2016-02-20 19:41:36 +0800

    add default list view for Office

--- offices/views.py
+++ offices/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from offices.models import Office
+
+class OfficeListView(ListView):
+    model = Office
+
+    def get_context_data(self, **kwargs):
+        context = super(OfficeListView, self).get_context_data(**kwargs)
+        return context
 
 # Create your views here.
