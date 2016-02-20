8184f4b8e986fba97eb6300c8f101c93d7d762ff
jethro guzman <jethroguzman@gmail.com>
2016-02-20 22:21:16 +0800

    add views for clients

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from clients.models import Client, Contact
+
+class ClientListView(ListView):
+    model = Client
+
+    def get_context_data(self, **kwargs):
+         context = super(ClientListView, self).get_context_data(**kwargs)
+         return context
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+    def get_context_data(self, **kwargs):
+         context = super(ClientDetailView, self).get_context_data(**kwargs)
+         return context
+
+
+class ContactDetailView(DetailView):
+    model = Contact
+
+    def get_context_data(self, **kwargs):
+         context = super(ContactDetailView, self).get_context_data(**kwargs)
+         return context
+
+
