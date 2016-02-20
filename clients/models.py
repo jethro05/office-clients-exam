8bab1d1e1370de60212bd899641c65f2f3c0955d
jethro guzman <jethroguzman@gmail.com>
2016-02-20 22:19:56 +0800

    add clients models

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from offices.models import Office
+
+
+class Client(models.Model):
+    office = models.ForeignKey(Office)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "[%s] %s" % (self.office, self.name)
+
+class Contact(models.Model):
+    client = models.ForeignKey(Client)
+    last_name = models.TextField()
+    first_name = models.TextField(blank=True)
+    email = models.TextField(blank=True)
+    
+    def _get_full_name(self):
+        return ('%s %s' % (self.first_name, self.last_name)).strip()
+
+    full_name = property(_get_full_name)
+
+    def __str__(self):
+        return self.full_name
